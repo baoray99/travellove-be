8f02e2f9dd804de8769e0f228e2e19123509d605
baoray99 <bao.ray1999@gmail.com>
2020-11-19 14:20:56 +0700

    deploy

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 require("./db/db");
 
+const port = process.env.PORT || 3000;
 const authRoute = require("./routes/auth");
 const placeRoute = require("./routes/place");
 
@@ -10,6 +11,6 @@ app.use(bodyParser.json());
 app.use("/users", authRoute);
 app.use("/places", placeRoute);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });

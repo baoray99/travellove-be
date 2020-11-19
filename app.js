39138d2c847d047878dcf7423e0b4ed032b8659c
baoray99 <bao.ray1999@gmail.com>
2020-11-19 23:31:50 +0700

    fix cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("./db/db");
 
 const port = process.env.PORT || 3500;
 const authRoute = require("./routes/auth");
 const placeRoute = require("./routes/place");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/users", authRoute);
 app.use("/places", placeRoute);

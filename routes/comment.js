8357b3be69b8916b3b8a2218177b89fc2c5d549b
Tan Le <lenhattan17tclc1@gmail.com>
2020-11-26 15:38:37 +0700

    routes comment

--- routes/comment.js
+++ routes/comment.js
@@ -6,5 +6,8 @@ const auth = require("../middleware/auth");
 router.post("/", auth, CommentController.createComment);
 router.get("/user/:userId", auth, CommentController.getAllByUser);
 router.get("/hotel/:ofWhatId", auth, CommentController.getAllByHotel);
+router.delete("/:_id",auth,CommentController.deleteComment);
+router.put("/:_id",auth,CommentController.updateComment);
+
 
 module.exports = router;

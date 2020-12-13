0269040fef77ac018173fc689afc1895bd854107
baoray99 <bao.ray1999@gmail.com>
2020-12-13 10:29:52 +0700

    fixxxxxxxxx

---
+++ controllers/favouritecontroller.js
@@ -0,0 +1,28 @@
+const Food = require("../models/food");
+const Hotel = require("../models/hotel");
+const HotPlace = require("../models/hotplace");
+
+const getFavourite = async (req, res) => {
+  try {
+    const foodFav = await Food.find({ users: req.params.userId }).select(
+      "name users placeId mainimg address"
+    );
+    const hotelFav = await Hotel.find({ users: req.params.userId }).select(
+      "name users placeId mainimg address"
+    );
+    const hotplaceFav = await HotPlace.find({
+      users: req.params.userId,
+    }).select("name users placeId mainimg address");
+    const fav = {
+      hotplaces: hotplaceFav,
+      foods: foodFav,
+      hotels: hotelFav,
+    };
+    res.json(fav);
+  } catch (error) {
+    res.json({ message: error });
+  }
+};
+module.exports = {
+  getFavourite,
+};
--- routes/favourite.js
+++ routes/favourite.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const FoodController = require("../controllers/foodcontroller");
-const HotelController = require("../controllers/hotelcontroller");
-const HotPlaceController = require("../controllers/hotplacecontroller");
+// const FoodController = require("../controllers/foodcontroller");
+// const HotelController = require("../controllers/hotelcontroller");
+// const HotPlaceController = require("../controllers/hotplacecontroller");
+const FavController = require("../controllers/favouritecontroller");
 const auth = require("../middleware/auth");
 
-router.get("/foods/:userId", auth, FoodController.getAllFoodByUser);
-router.get("/hotels/:userId", auth, HotelController.getAllHotelByUser);
-router.get("/hotplaces/:userId", auth, HotPlaceController.getAllHotPlaceByUser);
+// router.get("/foods/:userId", auth, FoodController.getAllFoodByUser);
+// router.get("/hotels/:userId", auth, HotelController.getAllHotelByUser);
+// router.get("/hotplaces/:userId", auth, HotPlaceController.getAllHotPlaceByUser);
+router.get("/:userId", auth, FavController.getFavourite);
 
 module.exports = router;
